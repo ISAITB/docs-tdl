f1edec22b9a3c3f0ca49fa2c7038eefa55a6b96b
simatosc <costas.simatos@sigmacubed.eu>
2025-03-13 17:14:26 +0100

    Gurubase update

--- source/conf.py
+++ source/conf.py
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 html_css_files = ['css/custom_2025.03.03.css']
-html_js_files = ['js/gurubase_2025.03.03.js']
+html_js_files = ['js/gurubase_2025.03.13.js']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
