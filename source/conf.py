a957dbe1a0b0a4b204e1a7f8b5273d5b25d2ce60
simatosc <costas.simatos@sigmacubed.eu>
2025-08-07 11:12:43 +0200

    Updates for release 1.27.2

--- source/conf.py
+++ source/conf.py
@@ -24,9 +24,9 @@ copyright = '2025, European Commission, DIGIT'
 author = 'European Commission, DIGIT'
 
 # The short X.Y version
-version = 'GITB TDL version 1.27.1'
+version = 'GITB TDL version 1.27.2'
 # The full version, including alpha/beta/rc tags
-release = 'GITB TDL version 1.27.1'
+release = 'GITB TDL version 1.27.2'
 
 html_show_sourcelink = False
 html_show_sphinx = False
